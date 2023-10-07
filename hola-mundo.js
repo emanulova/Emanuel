083f8cadfa36ca5a0057698e0eae49f54298df50
emanulova <143229410+emanulova@users.noreply.github.com>
2023-10-07 16:02:59 +0200

    primer JS

---
+++ hola-mundo.js
@@ -0,0 +1,2 @@
+alert("Hola Mundo, quieres ver la pagina? Dale en aceptar o te busco en tu casa por la noche"); console.log("Esto se muestra en la consola"); console.log(3+4);
+var nombre = prompt("Escribir tu nombre","")
