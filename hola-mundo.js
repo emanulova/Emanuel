64db23a4de6b1d73b8e5fb72d0fad65639b16f98
emanulova <143229410+emanulova@users.noreply.github.com>
2023-10-07 16:07:24 +0200

    hola-mundo.js

--- hola-mundo.js
+++ hola-mundo.js
@@ -1,3 +1,3 @@
 alert("Hola Mundo, quieres ver la pagina? Dale en aceptar o te busco en tu casa por la noche"); console.log("Esto se muestra en la consola"); console.log(3+4);
 var nombre = prompt("Escribir tu nombre","")
-alert("Hola"+ nombre);
+alert("Hola "+ nombre);
