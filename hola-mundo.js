62f8e645b6deade760b95ebf21ead0f98066dc00
emanulova <143229410+emanulova@users.noreply.github.com>
2023-10-28 03:11:50 -0300

    operaciones

--- hola-mundo.js
+++ hola-mundo.js
@@ -1,3 +1,15 @@
-alert("Hola Mundo, quieres ver la pagina? Dale en aceptar o te busco en tu casa por la noche"); console.log("Esto se muestra en la consola"); console.log(3+4);
+alert("Hola Mundo, quieres ver la pagina? Dale en aceptar"); console.log("Esto se muestra en la consola"); console.log(3+4);
 var nombre = prompt("Escribir tu nombre","")
-alert("Hola "+ nombre);
+alert("Bienvenido "+ nombre +" a la introduccion a la programacion");
+alert("A continuacion escribi 2 digitos para Sumarlos");
+var numero1 = prompt ("Primer numero ",)
+var numero2 = prompt ("Segundo numero ",)
+
+
+var Suma = numero1 + numero2
+var Resta = numero1 - numero2
+var Multiplicacion = numero1 * numero2
+alert("La suma es: " + Suma);
+alert("La Multiplicacion es: " + Multiplicacion);
+alert("La Resta es: " + Resta);
+
