7e2354709abd816e8f542376b924116f9b111dce
emanulova <143229410+emanulova@users.noreply.github.com>
2023-10-28 16:02:01 +0200

    hola-mundo.js

--- hola-mundo.js
+++ hola-mundo.js
@@ -1,15 +1,43 @@
-alert("Hola Mundo, quieres ver la pagina? Dale en aceptar"); console.log("Esto se muestra en la consola"); console.log(3+4);
+alert("Hola Mundo, quieres ver la pagina? Dale en aceptar"); 
+console.log("Esto se muestra en la consola"); 
+console.log(3+4);
+
 var nombre = prompt("Escribir tu nombre","")
 alert("Bienvenido "+ nombre +" a la introduccion a la programacion");
 alert("A continuacion escribi 2 digitos para Sumarlos");
-var numero1 = prompt ("Primer numero ",)
-var numero2 = prompt ("Segundo numero ",)
+var numero1 = Number(prompt ("Primer numero ",""));
+var numero2 = Number(prompt ("Segundo numero ",""));
 
 
 var Suma = numero1 + numero2
-var Resta = numero1 - numero2
-var Multiplicacion = numero1 * numero2
 alert("La suma es: " + Suma);
-alert("La Multiplicacion es: " + Multiplicacion);
+var Resta = numero1 - numero2
 alert("La Resta es: " + Resta);
+var Multiplicacion = numero1 * numero2
+alert("La Multiplicacion es: " + Multiplicacion);
+alert("A continuacion escribi 3 digitos para Promediarlo");
+var numero3 = Number(prompt ("Primer numero para Promedio",""));
+var numero4 = Number(prompt ("Segundo numero para Promedio",""))
+var numero5 = Number(prompt ("Tercer numero para Promedio",""))
+var Promedio = (numero3 + numero4 + numero5)/3
+alert("El Promedio es: " + Promedio);
+
+/* punto 1 de la actividad */
+var nombre = 'Emanuel';
+var edad = 16;
+var altura = 1.73;
+var hijos = false;
+
+console.log("Edad " + edad);
+console.log("Nombre " + nombre);
+console.log("Altura " + altura);
+console.log("Hijos " + hijos);
+
+/* falta IMC*/ 
+
+/* clase del 28/10 
+var edad = parseFloat(prompt("Ingrese la edad",""));
+var puedeEntrar = (edad>=18);
+alert("Puede ingresar? = " + puedeEntrar)
+*/
 
